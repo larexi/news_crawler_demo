21658c2a8edf01fb9dd6512c4a86a5b316e995d4
Lauri Kolehmainen <laurikolehmainen@Lauris-MBP.lan>
2023-05-02 20:32:03 +0300

    add dataclass

--- crawler.py
+++ crawler.py
@@ -1,20 +1,25 @@
 import asyncio
-from io import StringIO
+from dataclasses import dataclass
+from io import BytesIO
 
-from arsenic import browsers, get_session, services
+
+from typing import List
+import aiohttp
 from lxml import etree
+
+
+@dataclass
+class CrawlResult():
+    url: str
+    html: str
 
 class NewsCrawler():
     def __init__(self):
-        self.service = services.Chromedriver()
-        self.browser = browsers.Chrome(chromeOptions={
-            'args': ['--headless', '--disable-gpu']
-        })
         self.request_semaphore = asyncio.Semaphore(5)
 
     def _parse_etree_from_html(self, html):
         parser = etree.HTMLParser()
-        tree = etree.parse(StringIO(html), parser)
+        tree = etree.parse(BytesIO(html), parser)
         return tree
 
     def _parse_interesting_links_from_tree(self, tree):
@@ -57,23 +62,23 @@ class NewsCrawler():
 
         return interesting_links
 
-    def _parse_articles_from_htmls(self, htmls):
+    def _parse_articles_from_htmls(self, crawled_results: List[CrawlResult]):
         parsed_articles = []
-        for html in htmls:
+        for result in crawled_results:
             new_article = {
                 'content': '',
                 'headline': '',
                 'published': '',
-                'url': html[1]
+                'url': result.url
             }
 
-            html = html[0]
+            html = result.html
             tree = self._parse_etree_from_html(html)
             h1_elements = tree.xpath('//h1[contains(@class, "yle__article__heading")]')
             for elem in h1_elements:
                 new_article['headline'] += ' '.join(elem.itertext())
 
-            content_elements = tree.xpath('//div[@class="yle__article__content"]')
+            content_elements = tree.xpath('//section[@class="yle__article__content"]')
             for elem in content_elements:
                 new_article['content'] += ' '.join(elem.itertext())
 
@@ -95,27 +100,28 @@ class NewsCrawler():
                 continue
         return validated_links
 
-    async def _request_url(self, url, session):
+    async def _request_url(self, url, session) -> CrawlResult:
         async with self.request_semaphore:
-            await session.get(url)
-            html = await session.get_page_source()
-        return html, url
+            async with session.get(url) as response:
+                print(f"Requesting {url}")
+                html = await response.read()
+        return CrawlResult(url, html)
  
     async def _fetch_articles(self, urls):
-        async with get_session(self.service, self.browser) as session:
+        async with aiohttp.ClientSession() as session:
             tasks = []
             for url in urls:
                 tasks.append(
                     self._request_url(url, session)
                 )
-            htmls = await asyncio.gather(*tasks)
-        return htmls
+            crawled_urls = await asyncio.gather(*tasks)
+        return crawled_urls
 
     async def crawl_news(self, news_site_url):
-        async with get_session(self.service, self.browser) as session:
-            html, _ = await self._request_url(news_site_url, session)
+        async with aiohttp.ClientSession() as session:
+            crawl_result = await self._request_url(news_site_url, session)
 
-        tree = self._parse_etree_from_html(html)
+        tree = self._parse_etree_from_html(crawl_result.html)
         newslinks = self._parse_interesting_links_from_tree(tree)
         validated_newslinks = self._validate_links('https://yle.fi', newslinks)
         article_htmls = await self._fetch_articles(validated_newslinks)
@@ -128,4 +134,4 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     news = loop.run_until_complete(crawler.crawl_news(url))
     print(len(news))
-    print(news[0])
+    print(news)
